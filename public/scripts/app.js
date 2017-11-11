2584bfaf93752df057c8f1a7f1f5f360e1494a38
Russell Barthel <russell.barthel@gmail.com>
2017-11-10 16:51:25 -0800

    submit form functioning

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,30 +8,28 @@ $(function () {
     location = [sessionStorage.lat, sessionStorage.long];
   };
 
-  // poll the html5 location api
+  // poll the html5 location api when user clicks get location
   $('#getLocation').on('click', (event) => {
     event.preventDefault();
-    $('#getLocation').attr('disabled', 'disabled');
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLocation(Number(position.coords.latitude), Number(position.coords.longitude));
-      $('#location').hide();
-    }, (error) => {
-      console.error(error);
-      $('#getLocation').hide();
-    });
+    getLocation().then(() => {
+      $('#submit').removeAttr('disabled');
+    })
   });
 
   // set the location in local session storage
   function setLocation(lat, long) {
-    location = [lat, long];
-    sessionStorage.lat = location[0];
-    sessionStorage.long = location[1];
+    location = [lat, long]
+    sessionStorage.lat = lat;
+    sessionStorage.long = long;
   }
 
   // get location via google places autocomplete
   function googleLocation() {
     const place = autocomplete.getPlace();
-    setLocation(place.geometry.location.lat(), place.geometry.location.lng());
+    const lat = place.geometry.location.lat();
+    const long = place.geometry.location.lng();
+    setLocation(lat, long);
+    location = [lat, long];
     $('#location').hide();
   };
 
@@ -40,42 +38,91 @@ $(function () {
   const autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.addListener('place_changed', googleLocation);
 
-  // display price slider
-  $('#price').change(() => {
-    const priceVal = $('#price').val()
+  // set the display for min or max price
+  function setDisplayPrice(price, minOrMax) {
+    let priceSymbols = '';
+    for (let i = 0; i < price; i++) {
+      priceSymbols += '$';
+    }
+    $(`#display${minOrMax}Price`).text(priceSymbols);
+  }
 
-    if (priceVal === '0') {
-      $('#displayPrice').text('any')
-    } else {
-      let priceSymbols = '';
-      for (let i = 0; i < Number(priceVal); i++) {
-        priceSymbols += '$';
-      }
-      $('#displayPrice').text(priceSymbols);
+  // handle min price slider change
+  $('#minPrice').change(() => {
+    const minPrice = Number($('#minPrice').val());
+    const maxPrice = Number($('#maxPrice').val());
+    setDisplayPrice(minPrice, 'Min');
+    if (maxPrice > 0 && maxPrice < minPrice) {
+      $('#maxPrice').val(minPrice);
+      setDisplayPrice(minPrice, 'Max');
     }
   })
 
-  // display radius slider
+  // handle max price slider change
+  $('#maxPrice').change(() => {
+    const minPrice = Number($('#minPrice').val());
+    const maxPrice = Number($('#maxPrice').val());
+    setDisplayPrice(maxPrice, 'Max');
+    if (minPrice > 0 && minPrice > maxPrice) {
+      $('#minPrice').val(maxPrice);
+      setDisplayPrice(maxPrice, 'Min');
+    }
+  })
+
+  // handle radius slider change and set the display
   $('#radius').change(() => {
-    const radiusVal = $('#radius').val() + ' km';
+    radiusVal = $('#radius').val() + ' km';
     $('#displayRadius').text(radiusVal);
   })
 
-  function queryGoogle(params) {
-    console.log(params);
+  // get geo location
+  function getLocation() {
+    return new Promise((resolve, reject) => {
+      if (location) {
+        resolve();
+      } else {
+        $('#submit').attr('disabled', true);
+        $('#location').hide();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const lat = Number(position.coords.latitude);
+          const long = Number(position.coords.longitude);
+          setLocation(lat, long);
+          resolve();
+        }, (error) => {
+          $.get('http://freegeoip.net/json/', (results) => {
+            setLocation(results.latitude, results.longitude);
+            resolve();
+          });
+        });
+      };
+    })
   }
 
+  function queryGoogle(params) {
+    return new Promise((resolve, reject) => {
+      resolve(params);
+    });
+  }
+
+  // handler for submit
   $('#submit').on('click', (event) => {
     event.preventDefault();
-    if (location !== undefined) {
+    getLocation().then(() => {
       const params = {
         location: location,
-        // radius:
-        keyword: $('#keyword').val(),
-        // minPriceLevel: $('#priceLower').val(),
-        // maxPriceLevel: $('#priceUpper').val()
+        openNow: $('#openNow').is(':checked'),
+        keyword: $('#keyword').val() || 'restaurant',
+        radius: Number($('#radius').val()) * 1000,
       };
-      queryGoogle(params);
-    }
+      if
+      if (!($('#minPrice').val() === '0' && $('#maxPrice').val() === '4')) {
+        params.minPriceLevel = Number($('#minPrice').val());
+        params.maxPriceLevel = Number($('#maxPrice').val());
+      }
+      queryGoogle(params).then((results) => {
+        $('#submit').removeAttr('disabled');
+        console.log(results);
+      })
+    });
   });
 });
